4d264d3a529b8fe7d025f0284ec737c54ff55d83
quasar3c273 <hiimisha@mail.ru>
2021-04-04 04:26:46 +0300

    fix: advanced level: adjust according to rule 1.2 in html-and-css

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="delete-img";
+    deleteButtonImg.setAttribute('alt', 'delete entry');
     deleteButton.appendChild(deleteButtonImg);
 
 
