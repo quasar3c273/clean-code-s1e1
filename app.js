39d43fb99bf9f6691d6ee2bad39bcb810ade9623
quasar3c273 <hiimisha@mail.ru>
2021-04-04 12:19:56 +0300

    refactor: advanced level: adjust according to rule 2.1 in html-and-css

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
+var incompleteTaskHolder=document.querySelector(".incomplete-tasks");//ul of .incomplete-tasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
@@ -33,20 +33,20 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task item-label';
+    label.className='task item__label';
 
     //Each elements, needs appending
     listItem.className="item";
 
     checkBox.type="checkbox";
-    checkBox.className="item-checkbox";
+    checkBox.className="item__checkbox";
     editInput.type="text";
-    editInput.className="task item-text";
+    editInput.className="task task__text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="btn edit";
+    editButton.className="btn item__btn_edit";
 
-    deleteButton.className="btn delete";
+    deleteButton.className="btn item__btn_delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.className="delete-img";
     deleteButtonImg.setAttribute('alt', 'delete entry');
@@ -89,7 +89,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".item__btn_edit");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .edit-mode
     if(containsClass){
@@ -161,8 +161,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector("button.item__btn_edit");
+    var deleteButton=taskListItem.querySelector("button.item__btn_delete");
 
 
     //Bind editTask to edit button.
